742ac53589901ec651ce03e4fcb48dc129da4a65
emjorome <emjorome@espol.edu.ec>
2025-01-04 13:43:29 -0500

    eliminar planta

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -8,12 +8,12 @@ import { PLANTAS } from '../elements/plants';
   styleUrls: ['tab4.page.scss'],
   standalone: false,
 })
-export class Tab4Page implements OnInit{
+export class Tab4Page implements OnInit {
   plantas = PLANTAS;
 
   constructor(private router: Router) { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
   verDetalle(id: number) {
     this.router.navigate(['/tab5', id]).catch(error => {
@@ -23,6 +23,16 @@ export class Tab4Page implements OnInit{
 
   tomarFoto() {
     console.log('Función para tomar foto no implementada.');
+  }
+
+  eliminarPlanta(id: number) {
+    const index = this.plantas.findIndex((planta) => planta.id === id);
+    if (index !== -1) {
+      this.plantas.splice(index, 1); // Elimina la planta del arreglo
+      console.log(`Planta con ID ${id} eliminada.`);
+    } else {
+      console.error('Error: No se pudo encontrar la planta para eliminar.');
+    }
   }
 
 }
